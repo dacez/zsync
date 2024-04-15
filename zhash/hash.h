4110d08b0a5abcdfb0ab0e5d8d968809e495f11f
dacez <zhuodc@qq.com>
2024-04-15 18:50:48 +0800

    better hash

--- zhash/hash.h
+++ zhash/hash.h
@@ -3,25 +3,22 @@
 
 #include <stdint.h>
 
+// FNV-1a
 int64_t z_Hash(int8_t *data, int64_t len) {
-    int64_t hash = 5381;
-    int8_t c;
+  int64_t hash = 0xcbf29ce484222325; // FNV-1a 64位初始哈希值
+  int64_t fnv_prime = 0x100000001b3; // FNV-1a 64位质数
 
-    for (int64_t i = 0; i < len; ++i) {
-      c = data[i];
-      hash = ((hash << 5) + hash) + c;
-    }
-    return hash;
+  for (int64_t i = 0; i < len; i++) {
+    hash ^= (int64_t)data[i];
+    hash *= fnv_prime;
+  }
+
+  return hash;
 }
 
 int8_t z_Checksum(int8_t *data, int64_t len) {
   int64_t hash64 = z_Hash(data, len);
-  int8_t *hs = (int8_t*)&hash64;
-  int8_t hash8 = 0;
-  for (int8_t i = 0; i < 8; ++i) {
-    hash8 = hs[i] ^ hash8;
-  } 
-  return hash8;
+  return (int8_t)(hash64 & 0x00000000000000FF);
 }
 
 #endif
